bab6026c81d83604644c3aed19410a1b58146d99
sandipsaha597 <40121135+sandipsaha597@users.noreply.github.com>
2019-06-19 16:05:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 const btn = document.querySelector('.talk'),
   content = document.querySelector('.content')
 
+let greettings = [
+  "I am good, you little piece of shit",
+  "Doing good",
+  "Leave me alone",
+  "I hate you",
+  "Get a life"
+],
+  weather = [
+    "Why do you ask, you don't go out anyway.",
+    "weather is fine.",
+  ],
+  doNotKnow = [
+    'You are so alone. Try to go back home.',
+    'I have a boyfriend.',
+    'Leave me alone'
+    "You're awfully small to be so hugely irritating.",
+    'you are a mistake by your parent',
+    "you are burden of the earth."
+  ],
+  iLoveYou = [
+    "I hate you",
+    "You are too ugly",
+    "L O L",
+    'I have a boyfriend.',
+    "You have a very punchable face"
+  ],
+  slangs = 'fuck' || 'fucker' || 'bustard' || 'asshole' || 'shit',
+  hello = ['who are you?', 'I am busy', 'do not disturb me', 'what!']
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
 const recognition = new SpeechRecognition()
@@ -13,11 +41,13 @@ recognition.onresult = function(event) {
   const current = event.resultIndex
   const transcript = event.results[current][0].transcript
   content.textContent = transcript
-  readOutLoud(transcript)
+  readOutLoud("how are you")
 }
 
 btn.addEventListener('click', () => {
-  recognition.start()
+  // recognition.start()
+  readOutLoud("how are you")
+
 })
 
 // recognition.onspeechend = function() {
@@ -26,7 +56,31 @@ btn.addEventListener('click', () => {
 
 function readOutLoud(message) {
   const speech = new SpeechSynthesisUtterance()
-  speech.text = message
+  speech.text = doNotKnow[Math.floor(Math.random()* doNotKnow.length)]
+
+  if(message.includes("how are you")) {
+    const finalText = greettings[Math.floor(Math.random() * greettings.length )]
+    speech.text = finalText
+  } else if (message.includes("weather")) {
+    const finalText = weather[Math.floor(Math.random() * weather.length )]
+    speech.text = finalText 
+  } else if (message.includes("I love you")) {
+    const finalText = greettings[Math.floor(Math.random() * greettings.length )]
+    speech.text = finalText
+  } else if (message.includes(slangs)) {
+    const finalText = "go fuck your self. and marry your right hand."
+    speech.text = finalText
+  } else if (message == 'hi') {
+    const finalText = "bad bye. have the worst day of your life."
+    speech.text = finalText
+  } else if (message == 'hey' || 'hello') {
+    const finalText = hello[Math.floor(Math.random() * hello.length )]
+    speech.text = finalText
+  }
+
+
+
+
   speech.volume = 1
   speech.rate = 1
   speech.pitch = 1
